6c5df5962172cadda5bf6d8e0b51bb66f3955383
Knut Melvær <knut.melvaer@netlife.com>
2018-05-13 20:17:43 +0200

    hard code client config

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: process.env.projectId, // you can find this in sanity.json
-  dataset: process.env.dataset, // or the name you chose in step 1
+  projectId: 'anokeucs', // you can find this in sanity.json
+  dataset: 'production', // or the name you chose in step 1
   useCdn: true // `false` if you want to ensure fresh data
 })
