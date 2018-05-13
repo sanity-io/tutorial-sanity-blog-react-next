f3e79168ac2facdd3fb054891c5710581143f38d
Knut Melvær <knut.melvaer@netlife.com>
2018-05-13 20:39:26 +0200

    add index page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
-const Index = (props) => <div>Hello world!</div>
+import React from 'react'
+import Link from 'next/link'
+import client from '../client'
+
+const Index = ({ posts = [] }) => <div>
+  <h1>Welcome to a blog!</h1>
+  <ul>Posts
+      {
+          posts.map(({_id, title = '', slug = ''}) =>
+              slug && <li key={_id}>
+                <Link prefetch href={`/blog/${slug}`}><a>{title}</a></Link>
+              </li>
+          )
+      }
+  </ul>
+</div>
+
+Index.getInitialProps = async ({ query: { slug } }) => {
+    return { posts: await client.fetch(`*[_type == "post"][0..1000]{
+      _id,
+      title,
+      "slug": slug.current
+    }`) }
+}
 
 export default Index
