f256817952b3c6938b36b67ddab35d238dbea113
Knut Melvær <knut.melvaer@netlife.com>
2018-05-13 19:47:39 +0200

    put config in environment

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: 'INSERT_YOUR_PROJECT_ID', // you can find this in sanity.json
-  dataset: 'INSERT_DATASET_NAME', // or the name you chose in step 1
+  projectId: process.env.projectId, // you can find this in sanity.json
+  dataset: process.env.dataset, // or the name you chose in step 1
   useCdn: true // `false` if you want to ensure fresh data
 })
