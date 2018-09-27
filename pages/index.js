fc05504e5656a5d8d39972dfbc949d36080ee8c3
Knut Melvær <knut.melvaer@gmail.com>
2018-09-27 22:32:38 +0200

    Fix getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,29 @@
-import React from 'react'
-import Link from 'next/link'
-import { format } from 'date-fns'
-import client from '../client'
+import React from "react";
+import Link from "next/link";
+import { format } from "date-fns";
+import client from "../client";
 
-const Index = ({ posts = [] }) => <div>
-  <h1>Welcome to a blog!</h1>
-  <h2>Posts</h2>
+const Index = ({ posts = [] }) => (
+  <div>
+    <h1>Welcome to a blog!</h1>
+    <h2>Posts</h2>
 
-      {/*
-          posts.map(({_id, title = '', slug = '', _updatedAt = ''}) =>
-              slug && <li key={_id}>
-                <Link prefetch href={`/blog/${slug}`}><a>{title}</a></Link> ({format(_updatedAt, 'DD. MMMM, YYYY')})
-              </li>
-          ) */
-      }
+    {posts.map(
+      ({ _id, title = "", slug = "", _updatedAt = "" }) =>
+        slug && (
+          <li key={_id}>
+            <Link prefetch href={`/blog/${slug}`}>
+              <a>{title}</a>
+            </Link>{" "}
+            ({format(_updatedAt, "DD. MMMM, YYYY")})
+          </li>
+        )
+    )}
+  </div>
+);
 
-</div>
+Index.getInitialProps = async () => ({
+  posts: await client.fetch(`*[_type == "post"]`)
+});
 
-Index.getInitialProps = () => client.fetch(`*[_type == "post"]`)
-
-
-export default Index
+export default Index;
