3320296507fcf331bd949907481d9f3335260999
Knut Melvær <knut.melvaer@gmail.com>
2018-10-11 18:03:06 +0200

    Clean up

--- next.config.js
+++ next.config.js
@@ -3,18 +3,18 @@ const client = require('./client')
 module.exports = {
   exportPathMap: async function (defaultPathMap) {
     const paths = await client
-    .fetch('*[_type == "post" && defined(slug)].slug.current')
-    .then(data =>
-      data.reduce(
-        (acc, slug) => ({
-          '/': { page: '/' },
-          ...acc,
-          [`/blog/${slug}`]: { page: '/Blog/Post', query: { slug } }
-        }),
-        defaultPathMap
+      .fetch('*[_type == "post" && defined(slug)].slug.current')
+      .then(data =>
+        data.reduce(
+          (acc, slug) => ({
+            '/': { page: '/' },
+            ...acc,
+            [`/blog/${slug}`]: { page: '/Blog/Post', query: { slug } }
+          }),
+          defaultPathMap
         )
-        )
-        .catch(console.error)
+      )
+      .catch(console.error)
     return paths
   }
 }
