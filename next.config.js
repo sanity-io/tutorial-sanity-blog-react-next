47b10cd3224d71504c468d1698af82a037fc11e7
Knut Melvær <knut.melvaer@gmail.com>
2018-10-11 17:05:16 +0200

    Add defaultPathMap to export

--- next.config.js
+++ next.config.js
@@ -2,19 +2,19 @@ const client = require('./client')
 
 module.exports = {
   exportPathMap: async function (defaultPathMap) {
-    const path = await client
-      .fetch('*[_type == "post"].slug.current')
-      .then(data =>
-        data.reduce(
-          (acc, slug) => ({
-            '/': { page: '/' },
-            ...acc,
-            [`/blog/${slug}`]: { page: '/blog', query: { slug } }
-          }),
-          {}
+    const paths = await client
+    .fetch('*[_type == "post" && defined(slug)].slug.current')
+    .then(data =>
+      data.reduce(
+        (acc, slug) => ({
+          '/': { page: '/' },
+          ...acc,
+          [`/blog/${slug}`]: { page: '/Blog/Post', query: { slug } }
+        }),
+        defaultPathMap
         )
-      )
-      .catch(console.error)
-    return path
+        )
+        .catch(console.error)
+    return paths
   }
 }
