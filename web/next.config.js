48155527ad24e2c312f317d2c7eb4d539e28a1bf
kmelve <knut@netliferesearch.com>
2019-04-30 08:13:58 +0200

    add serverless target

--- web/next.config.js
+++ web/next.config.js
@@ -1,20 +1,21 @@
 const client = require('./client')
 
 module.exports = {
-  exportPathMap: async function (defaultPathMap) {
-    const paths = await client
-      .fetch('*[_type == "post" && defined(slug)].slug.current')
-      .then(data =>
-        data.reduce(
-          (acc, slug) => ({
-            '/': { page: '/' },
-            ...acc,
-            [`/p/${slug}`]: { page: '/post', query: { slug } }
-          }),
-          defaultPathMap
-        )
-      )
-      .catch(console.error)
-    return paths
-  }
+    target: 'serverless',
+    exportPathMap: async function(defaultPathMap) {
+        const paths = await client
+            .fetch('*[_type == "post" && defined(slug)].slug.current')
+            .then(data =>
+                data.reduce(
+                    (acc, slug) => ({
+                        '/': { page: '/' },
+                        ...acc,
+                        [`/p/${slug}`]: { page: '/post', query: { slug } }
+                    }),
+                    defaultPathMap
+                )
+            )
+            .catch(console.error)
+        return paths
+    }
 }
