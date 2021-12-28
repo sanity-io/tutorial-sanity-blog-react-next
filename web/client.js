06ce857fc7592765a3def872c5cb2bb67bcc27e0
Geoff Ball <geoff@gball.dev>
2021-12-28 11:23:08 -0700

    Update to Next 12

--- web/client.js
+++ web/client.js
@@ -1,10 +1,8 @@
-import { sanityClient } from '@sanity/client'
+const sanityClient = require('@sanity/client')
 
-const client = sanityClient({
+module.exports = sanityClient({
   projectId: '0wopf0m0', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
   apiVersion: '2021-12-28'
 })
-
-export default client
