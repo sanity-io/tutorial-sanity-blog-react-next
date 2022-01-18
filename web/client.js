af375bd6a24737da1107b923443d1e936259ce13
Geoff Ball <geoff@sanity.io>
2022-01-18 14:58:51 -0700

    Update repo and align with tutorial code (#15)

--- web/client.js
+++ web/client.js
@@ -3,5 +3,6 @@ const sanityClient = require('@sanity/client')
 module.exports = sanityClient({
   projectId: '0wopf0m0', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
-  useCdn: true // `false` if you want to ensure fresh data
+  useCdn: true, // `false` if you want to ensure fresh data
+  apiVersion: '2021-12-28'
 })
--- web/pages/index.js
+++ web/pages/index.js
@@ -2,19 +2,18 @@ import Link from 'next/link'
 import groq from 'groq'
 import client from '../client'
 
-function Index(props) {
-    const { posts = [] } = props
+const Index = ({posts}) => {
     return (
       <div>
         <h1>Welcome to a blog!</h1>
-        {posts.map(
-          ({ _id, title = '', slug = '', _updatedAt = '' }) =>
+        {posts.length > 0 && posts.map(
+          ({ _id, title = '', slug = '', publishedAt = '' }) =>
             slug && (
               <li key={_id}>
-                <Link prefetch href="/post/[slug]" as={`/post/${slug.current}`}>
+                <Link href="/post/[slug]" as={`/post/${slug.current}`}>
                   <a>{title}</a>
                 </Link>{' '}
-                ({new Date(_updatedAt).toDateString()})
+                ({new Date(publishedAt).toDateString()})
               </li>
             )
         )}
@@ -22,10 +21,15 @@ function Index(props) {
     )
 }
 
-Index.getInitialProps = async () => ({
-  posts: await client.fetch(groq`
-    *[_type == "post" && publishedAt < now()]|order(publishedAt desc)
-  `)
-})
+export async function getStaticProps() {
+    const posts = await client.fetch(groq`
+      *[_type == "post" && publishedAt < now()]|order(publishedAt desc)
+    `)
+    return {
+      props: {
+        posts
+      }
+    }
+}
 
 export default Index
