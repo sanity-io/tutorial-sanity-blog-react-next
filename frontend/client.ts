679f7e85b2aed0bf6cbeaaba86eaa89bd1ff3a4d
Logan <98792682+rlogank@users.noreply.github.com>
2023-03-26 10:53:43 -0400

    Update client.ts
    The default export of @sanity/client has been deprecated. Use the named export `createClient` instead.

--- frontend/client.ts
+++ frontend/client.ts
@@ -1,6 +1,6 @@
-import sanityClient from '@sanity/client'
+import { createClient } from '@sanity/client'
 
-export default sanityClient({
+export default createClient({
   projectId: 'your-project-id', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true // `false` if you want to ensure fresh data
